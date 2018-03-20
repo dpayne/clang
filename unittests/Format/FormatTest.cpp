82b48fa1640e7cb7876b38e67ffd89905da35fa8
dpayne <darby.payne@gmail.com>
2018-03-20 15:37:05 -0700

    Adding unit test for SpacesInParentheses with global namespace var

--- unittests/Format/FormatTest.cpp
+++ unittests/Format/FormatTest.cpp
@@ -8797,6 +8797,7 @@ TEST_F(FormatTest, ConfigurableSpacesInParentheses) {
   FormatStyle Spaces = getLLVMStyle();
 
   Spaces.SpacesInParentheses = true;
+  verifyFormat("do_something( ::globalVar );", Spaces);
   verifyFormat("call( x, y, z );", Spaces);
   verifyFormat("call();", Spaces);
   verifyFormat("std::function<void( int, int )> callback;", Spaces);
